552d4609210d42df981ac0112c445fe6e5a6ce6b
evieeangeline <62947222+evieeangeline@users.noreply.github.com>
2023-03-15 14:50:32 +1100

    updated team photo and linkedin

--- totus-landing-page/src/scenes/team/index.jsx
+++ totus-landing-page/src/scenes/team/index.jsx
@@ -6,15 +6,15 @@ const Team = () => {
         <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4">
 
             <TeamMemberIcon name="Borui Chen" role="Project Co-Lead" imageLoc="assets/img/team/borui.jpg" linkedInUrl="#" />
-            <TeamMemberIcon name="Evangeline Sturges" role="Project Co-Lead" imageLoc="assets/img/team/evangeline.jpg" linkedInUrl="#" />
+            <TeamMemberIcon name="Evangeline Sturges" role="Project Co-Lead" imageLoc="assets/img/team/Evangeline.jpg" linkedInUrl="https://www.linkedin.com/in/evangeline-sturges-206b3a199/" />
             <TeamMemberIcon name="Liam Smith" role="Communications" imageLoc="assets/img/team/liam.jpg" linkedInUrl="#" />
-            <TeamMemberIcon name="Daniel Cao" role="Vision Lead" imageLoc="assets/img/team/daniel.jpg" linkedInUrl="#" />
+            <TeamMemberIcon name="Daniel Cao" role="Vision Lead" imageLoc="assets/img/team/Daniel.jpg" linkedInUrl="#" />
         </div>
 
         <div class="row align-items-center mt-4 mb-5">
             <div class="col"></div>
-            <TeamMemberIcon name="Tirrn Gunawardana" role="Software Lead" imageLoc="assets/img/team/tirrn.jpg" linkedInUrl="#"/>   
-            <TeamMemberIcon name="Yuqi Wu" role="Mechanical Lead" imageLoc="assets/img/team/04.jpg" linkedInUrl="#"/>   
+            <TeamMemberIcon name="Tirrn Gunawardana" role="Software Lead" imageLoc="assets/img/team/Tirrn.jpg" linkedInUrl="#"/>   
+            <TeamMemberIcon name="Yuqi Wu" role="Mechanical Lead" imageLoc="assets/img/team/Yuqi.jpg" linkedInUrl="#"/>   
             <TeamMemberIcon name="Alexander Ollman" role="Founder & CEO" imageLoc="assets/img/team/04.jpg" linkedInUrl="#"/>   
             <div class="col"></div>
         </div>
